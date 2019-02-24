4bd48006b60c5ceb8883b81e8ef6633c746c5572
Adrian <acm248@alu.ua.es>
2019-02-24 21:00:49 +0100

    "Preparar" en rama master

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -8,6 +8,11 @@ namespace hada_p1
 {
     class HadaP1 ///Esta clase contiene metodos para convertir de segundos a minutos y viceversa
     {
+        /// <summary>
+        /// esta clase permite cambiar de segundos a minutos
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
         public static double Seconds2Minutes(double s)
         {
             double m = 0;
